9b17b190287305f8f495db1c32f17946cd279373
Kehan Fu <kehanfu@gmail.com>
2021-12-06 21:32:07 +0000

    finished assignment pt1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const PORT = 3005
+const express = require('express');
+const app = express();
+const PORT = 3005;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -9,6 +9,14 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello Player 1!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Welcome <b>Player 1!</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
