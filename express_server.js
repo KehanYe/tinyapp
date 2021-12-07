636c09bb542db7cb7c532bd6b7255b38cf9e3af1
Kehan Fu <kehanfu@gmail.com>
2021-12-07 00:30:37 +0000

    added partials and finished render templates

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3005;
 
 // set view engine to ejs
-app.set("view engine", "ej");
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,6 +12,17 @@ const urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello Player 1!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  //longurl is accessing the value of the key in URLDatabse object(url parameters is key)
+  res.render("urls_show", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
